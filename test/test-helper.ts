5f7fc1dab820b80a7470a1c7fa8904dbca200756
yogyy <yogimr261@gmail.com>
2024-10-05 15:24:08 +0700

    feat: add helper for get Cookie Value

--- test/test-helper.ts
+++ test/test-helper.ts
@@ -3,4 +3,11 @@ export const loginBody = {
   password: "herobrine100",
 };
 
+export function getCookieValue(name: string, cookieString: string) {
+  const value = `; ${cookieString}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift();
+  return null;
+}
+
 export const registerBody = { ...loginBody, name: "tester" };
