b37a56d5ee251d4b176886d6209a4fe15bd0849b
yogyy <yogimr261@gmail.com>
2024-10-22 20:51:29 +0700

    feat: add tes helper

--- test/test-helper.ts
+++ test/test-helper.ts
@@ -1,3 +1,5 @@
+import { expect } from "bun:test";
+
 export const loginBody = {
   email: "test@dev.local",
   password: "herobrine100",
@@ -9,5 +11,43 @@ export function getCookieValue(name: string, cookieString: string) {
   if (parts.length === 2) return parts.pop()?.split(";").shift();
   return null;
 }
+
+export const newNovel = {
+  title: "title example 200",
+  synopsis: "lorem200",
+  author: "yogyy",
+  banner: "", // must be valid url or empty string
+  cover: "", // must be valid url or empty string
+  genres: ["Action", "Fantasy", "Mysteries"],
+};
+
+export const updateNovel = {
+  ...newNovel,
+  title: "title updated 200",
+};
+
+export const newChapter = {
+  title: "chapter test",
+  novelId: "nvl_ybttnrfvjibegksk", // add novel id from seeding
+  chapterNum: 5,
+  content: "chapter 0000000000",
+};
+
+export const ExpectedNovel = expect.objectContaining({
+  id: expect.any(String),
+  title: expect.any(String),
+  author: expect.any(String),
+  genres: expect.any(Array),
+  synopsis: expect.any(String),
+  cover: expect.any(String) || null,
+  banner: expect.any(String) || null,
+  totalViews: expect.any(Number),
+  status: expect.any(String),
+  published_at: expect.any(String),
+  last_updated: expect.any(String),
+});
+
+export const cookieTest =
+  "auth_session=4pax6hqfujzhv4t3hsvqmsbrmyfmdw3yh3fxn4ph; HttpOnly; Max-Age=1209600; Path=/; SameSite=Lax";
 
 export const registerBody = { ...loginBody, name: "tester" };
