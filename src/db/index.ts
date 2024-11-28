ad074317cef49d0b3e480eccd05e54c18e9ddc6b
yogyy <yogimr261@gmail.com>
2024-11-29 00:06:55 +0700

    chore: update schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,9 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 import * as novelSchema from "./schema/novel";
 import * as userSchema from "./schema/user";
+import * as ntfSchema from "./schema/notification";
 
 const sqlite = new Database("./src/db/novel.sqlite");
-const db = drizzle(sqlite, { schema: { ...novelSchema, ...userSchema } });
+const db = drizzle(sqlite, { schema: { ...novelSchema, ...userSchema, ...ntfSchema } });
 
 export { db };
