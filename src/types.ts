f3b6582f138054b98442664fda9110f8d3c5ea97
yogyy <yogimr261@gmail.com>
2025-03-31 18:49:46 +0700

    chore: update type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
-import { Env } from "hono";
 import { Session, User } from "./db/schema/user";
 
 export interface AppContext extends Env {
+  Bindings: {
+    NODE_ENV: "development" | "production";
+  };
   Variables: {
     user: Omit<User, "passwordHash"> | null;
     session: Session | null;
