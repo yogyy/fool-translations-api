6f6576ae25ebae42bb8083b03aaafb7547d5f16d
yogyy <yogimr261@gmail.com>
2025-03-31 17:39:50 +0700

    fix: return 401 instead of 404 if non-admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,35 @@
 import { createMiddleware } from "hono/factory";
 import { getCookie } from "hono/cookie";
-import { AuthContext } from "@/types";
+import { AppContext } from "@/types";
 import { deleteSessionTokenCookie } from "./lib/session";
 import { SESSION_COOKIE_NAME, validateSessionToken } from "./lib/auth";
 
-export const isAdmin = createMiddleware<AuthContext>(async (c, next) => {
-  const admin = c.get("user")?.type === "admin";
-  if (!admin) return c.notFound();
-
-  await next();
-});
-
-export const isUser = createMiddleware<AuthContext>(async (c, next) => {
-  const user = c.get("user");
-  if (!user) return c.newResponse("", 401);
-
-  await next();
-});
-
-export const authentication = createMiddleware<AuthContext>(async (c, next) => {
-  const sessionId = getCookie(c, SESSION_COOKIE_NAME) ?? null;
+export const authentication = createMiddleware<AppContext>(async (c, next) => {
+  const sessionId = getCookie(c, SESSION_COOKIE_NAME);
   if (!sessionId) {
     c.set("user", null);
     c.set("session", null);
     return next();
   }
 
-  const { session, user } = await validateSessionToken(sessionId);
+  const { session, user } = await validateSessionToken(c.env, sessionId);
   if (!session) deleteSessionTokenCookie(c);
 
   c.set("user", user);
   c.set("session", session);
   return next();
+});
+
+export const isAdmin = createMiddleware<AppContext>(async (c, next) => {
+  const admin = c.get("user")?.type === "admin";
+  if (!admin) return c.json({ error: "Not an Admin" }, 401);
+
+  await next();
+});
+
+export const isUser = createMiddleware<AppContext>(async (c, next) => {
+  const user = c.get("user");
+  if (!user) return c.json({ error: "Unauthorized" });
+
+  await next();
 });
--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,9 +1,9 @@
-import { db } from "@/db";
+import { createDB } from "@/db";
 import { notificationTable } from "@/db/schema/notification";
 import { User } from "@/db/schema/user";
 import { NotifSeen } from "@/lib/dtos";
 import { isUser } from "@/middleware";
-import { AuthContext } from "@/types";
+import { AppContext } from "@/types";
 import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
@@ -13,29 +13,38 @@ const notAuthorized = {
   error: "You are not authorized to access this notification.",
 };
 
-const notificationRoutes = new Hono<AuthContext>()
+const notificationRoutes = new Hono<AppContext>()
   .use(isUser)
   .get("/", async (c) => {
     const user = c.get("user") as User;
+    const db = createDB(c.env);
     const ntf = await db.query.notificationTable.findMany({
       where: eq(notificationTable.userId, user?.id),
+      columns: { novelId: false },
+      with: { novel: { columns: { id: true, cover: true, title: true } } },
+      orderBy: (notificationTable, { desc }) => [desc(notificationTable.createdAt)],
     });
 
     return c.json({ success: true, data: ntf });
   })
-  .patch("/", zValidator("json", NotifSeen), async (c) => {
-    const { id } = c.req.valid("json");
+  .patch("/:id", zValidator("param", NotifSeen), async (c) => {
+    const { id } = c.req.valid("param");
     const user = c.get("user") as User;
+    const db = createDB(c.env);
 
     const ntf = await db.query.notificationTable.findFirst({ where: eq(notificationTable.id, id) });
     if (ntf?.userId !== user.id) return c.json(notAuthorized, 403);
-
-    await db.update(notificationTable).set({ isRead: true }).where(eq(notificationTable.id, id));
+    if (ntf.isRead) {
+      await db.update(notificationTable).set({ isRead: false }).where(eq(notificationTable.id, id));
+    } else {
+      await db.update(notificationTable).set({ isRead: true }).where(eq(notificationTable.id, id));
+    }
     return c.json({ success: true });
   })
-  .delete("/", zValidator("json", NotifSeen), async (c) => {
-    const { id } = c.req.valid("json");
+  .delete("/:id", zValidator("param", NotifSeen), async (c) => {
+    const { id } = c.req.valid("param");
     const user = c.get("user") as User;
+    const db = createDB(c.env);
 
     const ntf = await db.query.notificationTable.findFirst({ where: eq(notificationTable.id, id) });
     if (ntf?.userId !== user.id) return c.json(notAuthorized, 403);
